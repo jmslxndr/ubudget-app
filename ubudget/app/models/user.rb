d48c55d40b3107be453006aa6d7ab1ce22f57f35
NicholasGati <ngati016@hotmail.com>
2015-10-15 12:25:49 -0400

    added styling to tabs

--- ubudget/app/models/user.rb
+++ ubudget/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_one :financial
+  has_one :financial, dependent: :destroy
 
   after_create :make_financial
 
